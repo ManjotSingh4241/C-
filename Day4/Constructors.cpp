602ce60d68d65999e46ab092addc59b31408ac98
Manjot singh <82448988+ManjotSingh4241@users.noreply.github.com>
2023-02-14 18:28:05 -0500

    Add files via upload

---
+++ Day4/Constructors.cpp
@@ -0,0 +1,26 @@
+/****************Constructor******************/
+#include<iostream>
+using namespace std;
+
+class Complex {
+	int a, b;
+public:
+	Complex(void);
+	void printNumber() {
+		std::cout << "Your Number is: "<< a<<" + "<<b<<"i"<<endl;
+	}
+
+};
+
+Complex::Complex(void) {
+	a = 10;
+	b = 0;
+}
+
+int main() {
+
+	Complex c;
+	c.printNumber();
+
+	return 0; 
+}
